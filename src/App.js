b5c6bfc972848a7edc52b4afdad606cf900959f1
Carlos E. Feliz <74747182+xJokeer@users.noreply.github.com>
2021-11-27 19:46:10 -0400

    Update functionality for history

--- src/App.js
+++ src/App.js
@@ -12,12 +12,13 @@ class Game extends React.Component {
           squares: Array(9).fill(null),
         }
       ],
+      stepNumber: 0,
       xNext: true,
     };
   }
 
   handleClick(i){
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1]
     const squares = current.squares.slice();
     if(calculateWinner(squares) || squares[i]){
@@ -28,21 +29,29 @@ class Game extends React.Component {
         history: history.concat([{
             squares:squares,
         }]),
+        stepNumber: history.length,
         isNext: !this.state.isNext,       
       });
     
   }
 
+  jumpTo(step){
+    this.setState({
+      stepNumber: step,
+      isNext: (step % 2) === 0,
+    })
+  }
+
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
     const moves = history.map((step, move) => {
       const desc = move ?
         'Go to move #' + move :
         'Go to game start';
       return (
-        <li>
+        <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       );
